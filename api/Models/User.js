8f415ca962777d6df0fe4aa30ef41b0fcba616b5
Johannsephil <jay3pee@gmail.com>
2022-12-25 22:03:21 +0100

    add validation to User schema and add the custom error handler middleware to the application

--- api/Models/User.js
+++ api/Models/User.js
@@ -1,24 +1,34 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const { isEmail } = require("validator");
+const validator = require("validator");
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: [true, "Please specify a name"],
-    },
-    email: {
-        type: String,
-        required: [true, "Specify a mail address"],
-        validate: [isEmail, "Invalid Email"],
-    },
-    password: {
-        type: String,
-        required: [true],
-    },
-});
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: [true, "Please specify a name"],
+		},
+		email: {
+			type: String,
+			required: [true, "Specify a mail address"],
+		},
+		password: {
+			type: String,
+			required: [true, "Password is required"],
+			select: false,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc, ret) {
+				delete ret.password;
+				delete ret.__v;
+			},
+		},
+	}
+);
 
 const User = mongoose.model("User", userSchema);
 
-export default User;
+module.exports = User;
--- api/server.js
+++ api/server.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const { errorHandler, AuthError } = require("./Middlewares/errorhandler");
+const AuthRouter = require("./routes/AuthRoutes");
 
+const app = express();
 
-const app = express()
+require("dotenv").config();
 
-require('dotenv').config()
+const PORT = process.env.NODE_ENV || 3200;
 
-
-const PORT = process.env.NODE_ENV || 3200
-
-mongoose.connect(process.env.MONGO_URI_PROD);
+mongoose.connect(
+	!process.env.NODE_ENV || process.env.NODE_ENV !== "production"
+		? process.env.MONGO_URI_LOCAL
+		: process.env.MONGO_URI_PROD
+);
 const db = mongoose.connection;
-db.on('error',console.error.bind(console,'connection error'))
-db.on('open',()=>{
-    console.log('DB Connected successfully');
-})
+db.on("error", console.error.bind(console, "connection error"));
+db.on("open", () => {
+	console.log("DB Connected successfully");
+});
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
 
+app.use("/auth", AuthRouter);
+app.use((err, req, res, next) => {
+	errorHandler(err, res);
+});
 
-app.listen(PORT,()=>{
-    console.log('Served::::::::.....');
-})
+app.listen(PORT, () => {
+	console.log(`Served:::::::: on PORT:${PORT}`);
+});
